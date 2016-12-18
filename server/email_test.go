452317edd207fbe9fc5777e4d03f37805172e062
Matt Poegel <mpoegel@comcast.net>
2016-12-17 22:52:06 -0500

    added Mailer and email templates

---
+++ server/email_test.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func TestMailer(t *testing.T) {
+	Mailer, err := NewMailer("test@aicomp.io")
+	if err != nil {
+		log.Fatalln(err)
+		t.FailNow()
+	}
+	var toEmail string
+	if len(os.Args) > 2 {
+		toEmail = os.Args[2]
+	} else {
+		toEmail = os.Args[1]
+	}
+	Mailer.MailSignup(toEmail, "Test User")
+}
