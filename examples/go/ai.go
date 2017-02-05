5ab5d29f43b28eceb1d91c9b37bb5739f274b9d1
austindoeswork <wilsonaustin17@gmail.com>
2017-02-04 22:58:41 -0500

    wip

--- examples/go/ai.go
+++ examples/go/ai.go
@@ -11,20 +11,27 @@ func main() {
 	serverURL := "ws://localhost:9090/wsplay"
 	devkey := "ShanikasEasilyGoofyRamen"
 
+	//open websocket
 	var dialer *websocket.Dialer
-
 	conn, _, err := dialer.Dial(serverURL, nil)
 	checkErr(err)
+	defer conn.Close()
 
+	//write our devkey
 	err = conn.WriteMessage(1, []byte(devkey))
 	checkErr(err)
 
+	//receive acknowledgement
 	_, msg, err := conn.ReadMessage()
 	checkErr(err)
+	fmt.Printf("%s\n", msg)
 
-	fmt.Printf("welcome %s\n", msg)
-	// _ = conn
-	// _ = devkey
+	for {
+		_, msg, err = conn.ReadMessage()
+		checkErr(err)
+		fmt.Printf("%s\n", msg)
+		conn.WriteMessage(1, []byte("b00 01"))
+	}
 
 }
 
