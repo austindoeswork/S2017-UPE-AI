923b0f28613292711807bee8c4bbeb025961ed94
Darwin <dingd@rpi.edu>
2017-02-18 03:44:18 -0500

    Small fix to docs, changed deploy.sh to remove CREDENTIALS file

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ scp -r ./S2017-UPE-AI $1:~/npc/
 scp -r ./templates $1:~/npc/
 scp -r ./static $1:~/npc/
 
-scp ./dbinterface/CREDENTIALS $1:~/npc/dbinterface/
+# scp ./npc.conf $1:~/npc/npc.conf # should we really be deploying config file? not sure
 scp ./dbinterface/words/*.txt $1:~/npc/dbinterface/words/
 
 
